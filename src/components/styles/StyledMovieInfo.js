906ffc6a5e51404c461cf411c0f4c512b7273bc7
Andre Colon <andrecolon@gmail.com>
2021-07-21 09:20:45 -0700

    CSS fix - poster BG position change center to top

--- src/components/styles/StyledMovieInfo.js
+++ src/components/styles/StyledMovieInfo.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
-import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config';
+import styled from "styled-components";
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from "../../config";
 
 export const StyledMovieInfo = styled.div`
-  background: ${props =>
+  background: ${(props) =>
     props.backdrop
       ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.backdrop}')`
-      : '#000'};
+      : "#000"};
   background-size: cover !important;
-  background-position: center !important;
+  background-position: center top !important;
   width: 100%;
   padding: 40px 20px;
   box-sizing: border-box;
@@ -38,7 +38,7 @@ export const StyledMovieInfo = styled.div`
     overflow: hidden;
 
     h1 {
-      font-family: 'Abel', sans-serif;
+      font-family: "Abel", sans-serif;
       font-size: 48px;
       margin: 0;
 
@@ -54,7 +54,7 @@ export const StyledMovieInfo = styled.div`
     }
 
     p {
-      font-family: 'Abel', sans-serif;
+      font-family: "Abel", sans-serif;
       font-size: 18px;
       line-height: 26px;
     }
